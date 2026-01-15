3fa4d023aefcdb6ea9ddc1e0deaa7cfac6d1babc
Yasindu Navodh <cst21024@std.uwu.ac.lk>
2026-01-15 15:49:43 +0530

    complete header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,24 @@
 
 import React from 'react'
+import { assets } from '../assets/assets'
 
 function Header() {
   return (
-    <div>Header</div>
+      <div className='mx-8 sm:mx-16 xl:mx-24 relative'>
+          <div className='text-center mt-20 mb-8'>
+              
+              <div
+                className='inline-flex items-center justify-center gap-4 px-6 py-1.5 mb-4 border border-primary/40 bg-primary/10 rounded-full text-sm text-primary'>
+                  <p>New: AI feature Intergrated</p>
+                  <img
+                      src={assets.star_icon}
+                  className='w-2.5'>
+                  </img>
+
+              </div>
+          </div>
+          <img src={assets.gradientBackground} className='absolute -top-50 -z-1 opacity-7c0' />
+    </div>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,12 @@
+import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 
 export const HomePage = () => {
     
     return (
-      <NavBar />
+        <>
+        <NavBar />
+            <Header />
+            </>
     );
 }
