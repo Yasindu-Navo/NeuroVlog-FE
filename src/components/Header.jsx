b7b522879611c906764cc4ae56c4a99b0595eee7
Yasindu Navodh <cst21024@std.uwu.ac.lk>
2026-01-15 17:12:14 +0530

    Add hero section intro

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,8 +16,18 @@ function Header() {
                   </img>
 
               </div>
+        <h1
+          className='text-3xl sm:text-6xl font-semibold sm:leading-16 text-gray-700 '>Your own <span className='text-primary'>
+            blogging</span> <br />platform
+        </h1>
+
+        <p className='my-6 sm:my-8 max-w-2xl m-auto max-sm:text-xs text-gray-500'>
+          Turn moments into stories and ideas into impact.
+          Whether it's a single sentence or a full article, start creating with the power of AI by your side.
+        </p>
+        
           </div>
-          <img src={assets.gradientBackground} className='absolute -top-50 -z-1 opacity-7c0' />
+          <img src={assets.gradientBackground} className='absolute -top-50 -z-1 opacity-50' />
     </div>
   )
 }
