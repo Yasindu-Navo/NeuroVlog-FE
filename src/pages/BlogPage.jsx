c43c4696e2ae61246d1dde7dba82c5e6f3ed28b3
Yasindu Navodh <cst21024@std.uwu.ac.lk>
2026-01-16 17:35:28 +0530

    Add CSS fpr header section of blog page

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -25,14 +25,14 @@ const BlogPage = () => {
       <img src={assets.gradientBackground} alt='background image' className='absolute -top-50 -z-1 opacity-50' />
       <NavBar />
       
-      <div>
-        <p>Published on {moment(data.createdAt).format('MMMM Do YYYY')}</p>
-        <h1>{data.title }</h1>
-        <h2>{data.subTitle}</h2>
-        <p>Michael Brown</p>
+      <div className='text-center mt-20 text-gray-600'>
+        <p className='text-primary py-4 font-medium'>Published on {moment(data.createdAt).format('MMMM Do YYYY')}</p>
+        <h1 className='text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800'>{data.title }</h1>
+        <h2 className='my-5 max-w-lg truncate mx-auto'>{data.subTitle}</h2>
+        <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary'>Michael Brown</p>
       </div>
 
-      
+
       <div></div>
     </div>
 
