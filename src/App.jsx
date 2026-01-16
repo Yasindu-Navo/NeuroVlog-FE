46c682d68ef4cb97834ec1b10cd5bd92f05dcfaf
Yasindu Navodh <cst21024@std.uwu.ac.lk>
2026-01-16 23:03:07 +0530

    Add react routes for Admin Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import BlogPage from './pages/BlogPage'
+import Layout from './pages/admin/Layout'
+import Dashboard from './pages/admin/Dashboard'
+import AddBlog  from './pages/admin/AddBlog'
+import ListBlog  from './pages/admin/ListBlog'
+import Comments   from './pages/admin/Comments'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<HomePage/>} />
-        <Route path='/blog/:id' element={<BlogPage/>} />
+        <Route path='/blog/:id' element={<BlogPage />} />
+        <Route path='/admin' element={<Layout />} >
+              <Route index element={<Dashboard />} />
+              <Route path='addBlog' element={<AddBlog />} />
+              <Route path='listBlog' element={<ListBlog />} />
+              <Route path='comments' element={<Comments />} />
+             
+        
       </Routes>
     </div>
   )
