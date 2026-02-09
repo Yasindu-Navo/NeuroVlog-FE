3747f0c5726fd3f3a9dae27dad765b3fc0ac46d9
Yasindu Navodh <cst21024@std.uwu.ac.lk>
2026-02-09 19:58:00 +0530

    load the comments for the comment page

--- src/pages/admin/Comments.jsx
+++ src/pages/admin/Comments.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { comments_data } from "../../assets/assets";
+
 
 function Comments() {
+
+  const [comments, setComments] = useState('');
+  const [filter, setFilter] = useState('Not Approved');
+
+  const fetchComments = async () => {
+    setComments(comments_data);
+  }
+
+  useEffect(() => {
+    fetchComments();
+  },[])
+
+
   return (
-    <div>Comments</div>
+    <div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16  p-4 md:p-10 bg-blue-50/50">
+            
+    </div>
   )
 }
 
