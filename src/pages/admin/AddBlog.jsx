6e50014cd5e61320def3906d1d9c6c5a5e2f1a75
Yasindu Navodh <cst21024@std.uwu.ac.lk>
2026-02-10 20:38:01 +0530

    final update for FE
    reorganize the code

--- src/pages/admin/AddBlog.jsx
+++ src/pages/admin/AddBlog.jsx
@@ -103,16 +103,21 @@ function AddBlog() {
 
         <div className="flex gap-2 mt-4">
           <p>Publish now</p>
-          <input type="checkbox" checked={isPublished} className="scale-125 cursor-pointer"
-            onChange={e => setIsPublished(e.target.checked)}
-             />
-          
-            </div>
+          <input
+            type="checkbox"
+            checked={isPublished}
+            className="scale-125 cursor-pointer"
+            onChange={(e) => setIsPublished(e.target.checked)}
+          />
+        </div>
 
-        <button type="submit" className="mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm  ">Add Blog</button>
-
+        <button
+          type="submit"
+          className="mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm  "
+        >
+          Add Blog
+        </button>
       </div>
-
     </form>
   );
 }
--- src/pages/admin/Comments.jsx
+++ src/pages/admin/Comments.jsx
@@ -1,83 +1,83 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { comments_data } from "../../assets/assets";
 import { CommentTableItem } from "../../components/admin/CommentTableItem";
 
-
 function Comments() {
-
   const [comments, setComments] = useState([]);
-  const [filter, setFilter] = useState('Not Approved');
+  const [filter, setFilter] = useState("Not Approved");
 
   const fetchComments = async () => {
     setComments(comments_data);
-  }
+  };
 
   useEffect(() => {
     fetchComments();
-  },[])
-
+  }, []);
 
   return (
     <div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16  p-4 md:p-10 bg-blue-50/50">
-            
       <div className="flex justify-between items-center max-w-3xl">
         <h1>Comments</h1>
 
         <div className="flex gap-4">
-
           <button
-            onClick={() => setFilter('Approved')}
+            onClick={() => setFilter("Approved")}
             className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs
-              ${filter === 'Approved' ? 'text-primary' : 'text-gray-700'}`}
-          
+              ${filter === "Approved" ? "text-primary" : "text-gray-700"}`}
           >
             Approved
           </button>
 
-
-           <button
-            onClick={() => setFilter('Not Approved')}
+          <button
+            onClick={() => setFilter("Not Approved")}
             className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs
-              ${filter === 'Not Approved' ? 'text-primary' : 'text-gray-700'}`}
-          
+              ${filter === "Not Approved" ? "text-primary" : "text-gray-700"}`}
           >
             Not Approved
           </button>
-
         </div>
-
       </div>
 
       <div className="relative h-4/5 max-w-3xl overflow-x-auto mt-4 bg-white shadow rounded-lg scrollbar-hide">
         <table className="w-full text-sm text-gray-500">
           <thead className="text-xs text-gray-700 text-left uppercase">
             <tr>
-               <th scope="col" className="px-6 px-3">#</th>
-              <th scope="col" className="px-6 px-3">Blog Tittle & Comment</th>
-              <th scope="col" className="px-6 px-3 max-sm:hidden">Date</th>
-              <th scope="col" className="px-6 px-3">Action</th>
+              <th scope="col" className="px-6 px-3">
+                #
+              </th>
+              <th scope="col" className="px-6 px-3">
+                Blog Tittle & Comment
+              </th>
+              <th scope="col" className="px-6 px-3 max-sm:hidden">
+                Date
+              </th>
+              <th scope="col" className="px-6 px-3">
+                Action
+              </th>
             </tr>
-
           </thead>
 
           <tbody>
-
-            {comments.filter((comment) => {
-              if (filter === "Approved") return comment.isApproved === true;
-              return comment.isApproved === false;
-            }).map((comment, index) => {
-              return(
-                <CommentTableItem key={comment._id} index={index} comment={comment} fetchComments={fetchComments} />
-              )
-            })}
+            {comments
+              .filter((comment) => {
+                if (filter === "Approved") return comment.isApproved === true;
+                return comment.isApproved === false;
+              })
+              .map((comment, index) => {
+                return (
+                  <CommentTableItem
+                    key={comment._id}
+                    index={index}
+                    comment={comment}
+                    fetchComments={fetchComments}
+                  />
+                );
+              })}
           </tbody>
-
         </table>
-        
-</div>
-
+      </div>
     </div>
-  )
+  );
 }
 
-export default Comments
+export default Comments;
